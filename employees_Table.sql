282484a40337f7312c722c3e8c53db73a2f6b7ee
Izzyjoy1 <117804920+Izzyjoy1@users.noreply.github.com>
2026-04-23 05:48:58 +0100

    Add files via upload

---
+++ employees_Table.sql
@@ -0,0 +1,33 @@
+Drop DATABASE if exists QuickMart;
+CREATE DATABASE QuickMart;
+
+USE QuickMart;
+Drop table if exists employees;
+CREATE TABLE employees (
+    employee_ID VARCHAR(15) PRIMARY KEY,
+    employee_name VARCHAR(50),
+    department VARCHAR(30),
+    salary DECIMAL(12,2),
+    hire_date DATE);
+    
+    
+INSERT INTO employees VALUES ('IZ1001DS', 'Ayooluwa Abidogun', 'Technology Development', 5000000, '2026-01-02');
+INSERT INTO employees VALUES ('IZ1002DS', 'Peter Obi', 'Finance', 4000000, '2026-02-15');
+INSERT INTO employees VALUES ('IZ1003DS', 'Temiloluwa Elebiyo', 'Partnership', 4500000, '2026-01-11');
+INSERT INTO employees VALUES ('IZ1004DS', 'Christiana Osimiri', 'Technology Development', 3000000, '2026-04-03');
+INSERT INTO employees VALUES ('IZ1005DS', 'Ademola Ajagbe', 'Online Merchant', 3000000, '2026-04-15');
+INSERT INTO employees VALUES ('IZ1006DS', 'Faith Etim', 'Online Merchant', 2500000, '2026-03-07');
+INSERT INTO employees VALUES ('IZ1007DS', 'Adeyinka Lawal', 'Sales', 2700000, '2026-02-05');
+INSERT INTO employees VALUES ('IZ1008DS', 'Emmanuel Sodimu', 'UI/UX', 700000, '2026-02-18');
+INSERT INTO employees VALUES ('IZ1009DS', 'Temitope Adeyemi', 'Human Resources', 1000000, '2026-03-14');
+INSERT INTO employees VALUES ('IZ1010DS', 'Blessing Idowu', 'Sales', 1100000, '2026-04-18');
+INSERT INTO employees VALUES ('IZ1011DS', 'Victoria Adewumi', 'Wealth', 2000000, '2026-03-06');
+INSERT INTO employees VALUES ('IZ1012DS', 'Cynthia Dooyum', 'Sales', 650000, '2026-01-14');
+INSERT INTO employees VALUES ('IZ1013DS', 'Francisca Joledo', 'Technology Development', 2000000, '2026-03-15');
+INSERT INTO employees VALUES ('IZ1014DS', 'Kemi Oladosu', 'Customer Service', 768000, '2026-04-24');
+INSERT INTO employees VALUES ('IZ1015DS', 'David Faniyi', 'Sales', 500000, '2026-04-03');
+INSERT INTO employees VALUES ('IZ1016DS', 'Mofere Adekanye', 'Wealth', 1700000, '2026-01-20');
+INSERT INTO employees VALUES ('IZ1017DS', 'Precious Akinpelu', 'Customer Service', 300000, '2026-02-01');
+INSERT INTO employees VALUES ('IZ1018DS', 'Emeka Mpamah', 'Sales', 920000, '2026-01-15');
+INSERT INTO employees VALUES ('IZ1019DS', 'Victor Adeyemo', 'Sales', 410000, '2026-01-17');
+INSERT INTO employees VALUES ('IZ1020DS', 'Qudus Yusuf', 'Customer Service', 1600000,'2026-04-21');
